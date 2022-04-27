a24a375972cad8e7ef3758082cbcd0e2eacf2ff8
Muniox <muniox20@gmail.com>
2022-04-28 00:07:18 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/main.scss';
+// import costam from './assets/costam.svg';
+
+ReactDOM.render(<App />, document.getElementById('root'));
