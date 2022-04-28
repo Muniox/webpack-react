f1407d2ed8acdf73f901ccfb5f949dbf4e0570ac
Muniox <muniox20@gmail.com>
2022-04-28 13:13:50 +0200

    DEV State

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import endsWith from './endsWith';
+import 'core-js/actual/array/from';       // <- at the top of your entry point
+import 'core-js/actual/array/group-by';   // <- at the top of your entry point
+import 'core-js/actual/set';              // <- at the top of your entry point
+import 'core-js/actual/promise';          // <- at the top of your entry point
+import 'core-js/actual/structured-clone'; // <- at the top of your entry point
+import 'core-js/actual/queue-microtask';  // <- at the top of your entry point
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/main.scss';
-// import costam from './assets/costam.svg';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,80 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js')
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].[contenthash].js",
-        clean: true,
-        assetModuleFilename: "[name][ext]",
-    },
-    devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist')
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: { postcssOptions: { plugins: ['autoprefixer'] } },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { 
+              "useBuiltIns": "usage",
+              "corejs": 3,
+              "targets": {
+                "browsers": [
+                  "edge >= 16",
+                  "safari >= 9",
+                  "firefox >= 57",
+                  "ie >= 10",
+                  "ios >= 9",
+                  "chrome >= 49"
+                ]
+              }
+            }], '@babel/preset-react']
+          },
         },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack App",
-            filename: "index.html",
-            template: "src/template.html"
-        }),
-        // new BundleAnalyzerPlugin()
-    ]
-}
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+    // new BundleAnalyzerPlugin()
+  ],
+};
