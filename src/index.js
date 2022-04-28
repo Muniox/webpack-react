4ebef8465fe6b71cc9708ab768c5448123a16669
Muniox <muniox20@gmail.com>
2022-04-28 14:24:25 +0200

    IE 11 work only in dist, not in dev server mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
+import 'core-js/features/string/ends-with';
+// import 'core-js/actual';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import endsWith from './endsWith';
-import 'core-js/actual/array/from';       // <- at the top of your entry point
-import 'core-js/actual/array/group-by';   // <- at the top of your entry point
-import 'core-js/actual/set';              // <- at the top of your entry point
-import 'core-js/actual/promise';          // <- at the top of your entry point
-import 'core-js/actual/structured-clone'; // <- at the top of your entry point
-import 'core-js/actual/queue-microtask';  // <- at the top of your entry point
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
